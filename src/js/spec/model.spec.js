49cec5cc52045a442998f02e8fb5967a6a762221
Eryk Napierała <eryk.piast@gmail.com>
2015-04-21 22:24:01 +0000

    add tests for model

--- src/js/spec/model.spec.js
+++ src/js/spec/model.spec.js
@@ -93,6 +93,159 @@ suite('model', () => {
             }));
 
         });
+        
+        
+        suite('areAutocompletionsVisible$', () => {
+            
+            test('should be hidden by default',
+            inject((createObservable, onNext, callWithObservables, getValues) => {
+                let areAutocompletionsVisible$ = callWithObservables(model.areAutocompletionsVisible$, { });
+
+                let results = getValues(areAutocompletionsVisible$);
+
+                assert.equalCollection(
+                    results,
+                    [ false ]
+                );
+
+            }));
+
+            test('should hide autocompletions when there is nothing to show',
+            inject((createObservable, onNext, callWithObservables, getValues) => {
+                let areAutocompletionsVisible$ = callWithObservables(model.areAutocompletionsVisible$, {
+                    autocompletions$: createObservable(
+                        onNext(100, [ 'definition', 'undefined' ]),
+                        onNext(200, [ ]),
+                        onNext(250, [ 'abc' ])
+                    )
+                });
+
+                let results = getValues(areAutocompletionsVisible$);
+
+                assert.equalCollection(
+                    results,
+                    [ false, true, false, true ]
+                );
+
+            }));
+
+
+            test('should hide autocompletions when show request is performed and hide when hide request is performed',
+            inject((createObservable, onNext, callWithObservables, getValues) => {
+                let areAutocompletionsVisible$ = callWithObservables(model.areAutocompletionsVisible$, {
+                    showAutocompletions$: createObservable(
+                        onNext(100, [ 'something' ]),
+                        onNext(200, [ 'anything' ])
+                    ),
+                    hideAutocompletions$: createObservable(
+                        onNext(150, [ 'something' ]),
+                        onNext(250, [ 'anything' ])
+                    )
+                });
+
+                let results = getValues(areAutocompletionsVisible$);
+
+                assert.equalCollection(
+                    results,
+                    [ false, true, false, true, false ]
+                );
+
+            }));
+
+
+            test('should emit value only if it is different than previous one',
+            inject((createObservable, onNext, callWithObservables, getValues) => {
+                let areAutocompletionsVisible$ = callWithObservables(model.areAutocompletionsVisible$, {
+                    autocompletions$: createObservable(
+                        onNext(101, [ 'definition', 'undefined' ]),
+                        onNext(199, [ ]),
+                        onNext(250, [ 'abc' ]),
+                        onNext(251, [ ]),
+                    ),
+                    showAutocompletions$: createObservable(
+                        onNext(100, [ 'something' ]),
+                        onNext(200, [ 'anything' ])
+                    ),
+                    hideAutocompletions$: createObservable(
+                        onNext(150, [ 'something' ]),
+                        onNext(250, [ 'anything' ])
+                    )
+                });
+
+                let results = getValues(areAutocompletionsVisible$);
+
+                assert.equalCollection(
+                    results,
+                    [ false, true, false, true, false ]
+                );
+
+            }));
+
+        });
+        
+        
+        // suite('highlightedAutocompletionIndex$', () => {
+
+        //     test('should reset index to 0 every time value changes',
+        //     inject((createObservable, onNext, callWithObservables, getValues) => {
+        //         let highlightedAutocompletionIndex$ = callWithObservables(model.highlightedAutocompletionIndex$, {
+        //             valueChange$: createObservable(
+        //                 onNext(100, 'something'),
+        //                 onNext(200, 'anything')
+        //             ),
+        //             autocompletions$: [ 'abc1', 'abc2', 'adef', 'axyz' ]
+        //         });
+
+        //         let results = getValues(highlightedAutocompletionIndex$);
+
+        //         assert.equal(results.length, 3);
+
+        //     }));
+
+
+        //     test('should change value every time datalist changes',
+        //     inject((createObservable, onNext, callWithObservables, getValues) => {
+        //         let autocompletions$ = callWithObservables(model.autocompletions$, {
+        //             valueChange$: 'abc',
+        //             datalistAttr$: createObservable(
+        //                 onNext(100, [ [ 'abc1' ], [ 'abc2' ], [ 'def' ], [ 'xyz' ] ]),
+        //                 onNext(200, [ [ 'abc2' ], [ 'def' ] ]),
+        //                 onNext(250, [ [ 'abc1' ], [ 'xyz' ] ])
+        //             )
+        //         });
+
+        //         let results = getValues(autocompletions$);
+
+        //         assert.equal(results.length, 3);
+
+        //     }));
+
+
+        //     test('should show only autocompletions matching with current value',
+        //     inject((createObservable, onNext, callWithObservables, getValues) => {
+        //         let autocompletions$ = callWithObservables(model.autocompletions$, {
+        //             valueChange$: createObservable(
+        //                 onNext(100, 'abc'),
+        //                 onNext(200, 'def'),
+        //                 onNext(250, 'ghi')
+        //             ),
+        //             datalistAttr$: [ [ 'abc1' ], [ 'abc2' ], [ 'definition' ], [ 'undefined' ], [ 'xyz' ] ]
+        //         });
+
+        //         let results = getValues(autocompletions$);
+
+        //         assert.equalCollection(
+        //             results,
+        //             [
+        //                 [ 'abc1', 'abc2' ],
+        //                 [ 'definition', 'undefined' ],
+        //                 [ ]
+        //             ]
+        //         );
+
+        //     }));
+
+        // });
 
 
         // suite('selectedAutocompletionInput$', () => {
